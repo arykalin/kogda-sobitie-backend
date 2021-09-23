d747d01f3cc79ed5c4cec0922b79c3f33044f6de
Aleksandr Rykalin <arykalin@gmail.com>
2021-09-23 19:18:45 +0300

    renaming

--- go.mod
+++ go.mod
@@ -7,16 +7,18 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/fatih/color v1.10.0
 	github.com/go-playground/universal-translator v0.17.0 // indirect
-	github.com/golang/snappy v0.0.2 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
 	github.com/klauspost/compress v1.11.2 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/rs/cors v1.7.0
 	go.mongodb.org/mongo-driver v1.4.2
+	go.uber.org/zap v1.19.1
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
-	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9 // indirect
-	golang.org/x/text v0.3.4 // indirect
+	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f
+	google.golang.org/api v0.57.0
+	gopkg.in/Iwark/spreadsheet.v2 v2.0.0-20191122095212-08231195c43b
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v9 v9.31.0
 )
--- tests/create_events_from_table.go
+++ tests/create_events_from_table.go
@@ -3,14 +3,62 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"strings"
+
+	"github.com/arykalin/kogda-sobitie-backend/tests/migration/events"
+	"go.uber.org/zap"
+
+	sheet "github.com/arykalin/kogda-sobitie-backend/tests/migration/scheet"
 )
 
 func main() {
 
 	url := "http://localhost:8080/event"
 	method := "POST"
+
+	sLoggerConfig := zap.NewDevelopmentConfig()
+	sLoggerConfig.DisableStacktrace = true
+	sLoggerConfig.DisableCaller = true
+	sLogger, err := sLoggerConfig.Build()
+	if err != nil {
+		panic(err)
+	}
+	logger := sLogger.Sugar()
+
+	s, err := sheet.NewSheetService(logger)
+	if err != nil {
+		log.Fatalf("failed to init sheet client: %s", err)
+	}
+
+	spreadsheet, err := s.GetSheet("14lqlXGtiT6vHi6O4X3iGDy9fATI3kghb8bS1_szNA_o")
+	if err != nil {
+		logger.Fatalf("failed to get sheet data: %s", err)
+	}
+
+	gotSheet, err := spreadsheet.SheetByIndex(0)
+	if err != nil {
+		logger.Fatalf("failed to get sheet data: %s", err)
+	}
+
+	sheetConfig := events.SheetConfig{
+		DateIdx:        1,
+		TitleIdx:       3,
+		DescriptionIdx: 9,
+		DurationIdx:    2,
+		LinkIdx:        4,
+		WhoManagesIdx:  5,
+		ForWhomIdx:     6,
+		WhereIdx:       8,
+		Skip:           2,
+	}
+
+	e := events.NewEvents()
+	err = e.AddEvents(gotSheet, &sheetConfig)
+	if err != nil {
+		logger.Fatalf("failed to add events: %s", err)
+	}
 
 	payload := strings.NewReader(`{
     "date":"24.09.2021",
