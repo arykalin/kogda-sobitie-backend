caf2d47479ea29b943458cb0e4425cc290d70a18
Aleksandr Rykalin <arykalin@gmail.com>
2021-09-23 13:54:20 +0300

    fix env file location

--- handlers/config.go
+++ handlers/config.go
@@ -11,7 +11,7 @@ import (
 func DotEnvVariable(key string) string {
 
 	// load .env file
-	err := godotenv.Load("../.env")
+	err := godotenv.Load(".env")
 
 	if err != nil {
 		log.Fatalf("Error loading .env file")
