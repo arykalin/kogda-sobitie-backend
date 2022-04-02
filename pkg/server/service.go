f8b998a3fe1aa4f6c476d4141c9e2812a4c8387c
Aleksandr Rykalin <arykalin@gmail.com>
2022-04-03 01:59:27 +0300

    making grpc

--- pkg/server/service.go
+++ pkg/server/service.go
@@ -9,25 +9,33 @@ import (
 	grpcHandler "github.com/arykalin/kogda-sobitie-backend/pkg/server/v1/grpc/handlers"
 	grpcModels "github.com/arykalin/kogda-sobitie-backend/pkg/server/v1/grpc/models"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Service struct {
-	grpcServer *grpc.Server
-	ctx        context.Context
-	mux        *runtime.ServeMux
+	grpcServer         *grpc.Server
+	ctx                context.Context
+	mux                *runtime.ServeMux
+	opts               []grpc.DialOption
+	grpcServerEndpoint *string
 }
 
 func (s *Service) Start() error {
+	var group errgroup.Group
+
 	lis, err := net.Listen("tcp", ":8842")
 	if err != nil {
 		return err
 	}
-	err = s.grpcServer.Serve(lis)
-	if err != nil {
-		return err
-	}
+	group.Go(func() error {
+		return s.grpcServer.Serve(lis)
+	})
+	group.Go(func() error {
+		return grpcModels.RegisterApiServiceHandlerFromEndpoint(s.ctx, s.mux, *s.grpcServerEndpoint, s.opts)
+	})
+
 	err = http.ListenAndServe(":8081", s.mux)
 	if err != nil {
 		return err
@@ -42,15 +50,13 @@ func NewService() (*Service, error) {
 
 	mux := runtime.NewServeMux()
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-	var grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:9090", "gRPC server endpoint")
-	err := grpcModels.RegisterApiServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
-	if err != nil {
-		return nil, err
-	}
+	grpcServerEndpoint := flag.String("grpc-server-endpoint", "localhost:9090", "gRPC server endpoint")
 
 	return &Service{
-		grpcServer: grpcServer,
-		mux:        mux,
-		ctx:        ctx,
+		grpcServer:         grpcServer,
+		mux:                mux,
+		ctx:                ctx,
+		opts:               opts,
+		grpcServerEndpoint: grpcServerEndpoint,
 	}, nil
 }
