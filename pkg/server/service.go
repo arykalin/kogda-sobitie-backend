31054b7d301c817eab63a57425376f6c8c97d614
Aleksandr Rykalin <arykalin@gmail.com>
2022-04-03 01:49:57 +0300

    making grpc

--- pkg/server/service.go
+++ pkg/server/service.go
@@ -1,17 +1,56 @@
 package server
 
 import (
+	"context"
+	"flag"
+	"net"
+	"net/http"
+
+	grpcHandler "github.com/arykalin/kogda-sobitie-backend/pkg/server/v1/grpc/handlers"
 	grpcModels "github.com/arykalin/kogda-sobitie-backend/pkg/server/v1/grpc/models"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-type Service struct{}
-
-func (s *Service) Start() {
-	var grpcServer = grpc.NewServer()
-	grpcServer.RegisterService(&grpcModels.ApiService_ServiceDesc, grpcHalers)
+type Service struct {
+	grpcServer *grpc.Server
+	ctx        context.Context
+	mux        *runtime.ServeMux
 }
 
-func NewService() *Service {
-	return &Service{}
+func (s *Service) Start() error {
+	lis, err := net.Listen("tcp", ":8842")
+	if err != nil {
+		return err
+	}
+	err = s.grpcServer.Serve(lis)
+	if err != nil {
+		return err
+	}
+	err = http.ListenAndServe(":8081", s.mux)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewService() (*Service, error) {
+	ctx := context.Background()
+	var grpcServer = grpc.NewServer()
+	grpcServer.RegisterService(&grpcModels.ApiService_ServiceDesc, grpcHandler.NewHandler())
+
+	mux := runtime.NewServeMux()
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	var grpcServerEndpoint = flag.String("grpc-server-endpoint", "localhost:9090", "gRPC server endpoint")
+	err := grpcModels.RegisterApiServiceHandlerFromEndpoint(ctx, mux, *grpcServerEndpoint, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Service{
+		grpcServer: grpcServer,
+		mux:        mux,
+		ctx:        ctx,
+	}, nil
 }
--- pkg/server/v1/grpc/handlers/handler.go
+++ pkg/server/v1/grpc/handlers/handler.go
@@ -11,15 +11,25 @@ type handler struct {
 	grpcModels.UnimplementedApiServiceServer
 }
 
-//type ApiServiceServer interface {
-//	Authenticate(context.Context, *grpcModels.AuthenticateRequest) (*grpcModels.AuthenticateResponse, error)
-//	CreateEvent(context.Context, *grpcModels.CreateEventRequest) (*grpcModels.CreateEventResponse, error)
-//	GetEvent(context.Context, *grpcModels.GetEventRequest) (*grpcModels.GetEventResponse, error)
-//	DeleteEvent(context.Context, *grpcModels.DeleteEventRequest) (*grpcModels.DeleteEventResponse, error)
-//	UpdateEvent(context.Context, *grpcModels.UpdateEventRequest) (*grpcModels.UpdateEventResponse, error)
-//	ListEvents(context.Context, *grpcModels.ListEventsRequest) (*grpcModels.ListEventsResponse, error)
-//}
-
 func (h handler) Authenticate(context.Context, *grpcModels.AuthenticateRequest) (*grpcModels.AuthenticateResponse, error) {
 	return nil, nil
+}
+func (h handler) CreateEvent(context.Context, *grpcModels.CreateEventRequest) (*grpcModels.CreateEventResponse, error) {
+	return nil, nil
+}
+func (h handler) GetEvent(context.Context, *grpcModels.GetEventRequest) (*grpcModels.GetEventResponse, error) {
+	return nil, nil
+}
+func (h handler) DeleteEvent(context.Context, *grpcModels.DeleteEventRequest) (*grpcModels.DeleteEventResponse, error) {
+	return nil, nil
+}
+func (h handler) UpdateEvent(context.Context, *grpcModels.UpdateEventRequest) (*grpcModels.UpdateEventResponse, error) {
+	return nil, nil
+}
+func (h handler) ListEvents(context.Context, *grpcModels.ListEventsRequest) (*grpcModels.ListEventsResponse, error) {
+	return nil, nil
+}
+
+func NewHandler() grpcModels.ApiServiceServer {
+	return handler{}
 }
