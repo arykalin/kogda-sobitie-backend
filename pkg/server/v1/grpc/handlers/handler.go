0e210170d2f88ef44b2a8dca1d72ed9f3667ec01
Aleksandr Rykalin <arykalin@gmail.com>
2022-04-03 01:11:13 +0300

    making grpc

--- pkg/server/v1/grpc/handlers/handler.go
+++ pkg/server/v1/grpc/handlers/handler.go
@@ -1,10 +1,25 @@
 package handlers
 
 import (
+	"context"
+
 	grpcModels "github.com/arykalin/kogda-sobitie-backend/pkg/server/v1/grpc/models"
 )
 
 type handler struct {
 	event
 	grpcModels.UnimplementedApiServiceServer
+}
+
+//type ApiServiceServer interface {
+//	Authenticate(context.Context, *grpcModels.AuthenticateRequest) (*grpcModels.AuthenticateResponse, error)
+//	CreateEvent(context.Context, *grpcModels.CreateEventRequest) (*grpcModels.CreateEventResponse, error)
+//	GetEvent(context.Context, *grpcModels.GetEventRequest) (*grpcModels.GetEventResponse, error)
+//	DeleteEvent(context.Context, *grpcModels.DeleteEventRequest) (*grpcModels.DeleteEventResponse, error)
+//	UpdateEvent(context.Context, *grpcModels.UpdateEventRequest) (*grpcModels.UpdateEventResponse, error)
+//	ListEvents(context.Context, *grpcModels.ListEventsRequest) (*grpcModels.ListEventsResponse, error)
+//}
+
+func (h handler) Authenticate(context.Context, *grpcModels.AuthenticateRequest) (*grpcModels.AuthenticateResponse, error) {
+	return nil, nil
 }
