3dafdcfd6c0056924bd6e43464d35857cebe038d
Aleksandr Rykalin <arykalin@gmail.com>
2022-09-15 18:36:23 +0300

    making grpc

--- pkg/server/v1/grpc/handlers/handler.go
+++ pkg/server/v1/grpc/handlers/handler.go
@@ -28,18 +28,42 @@ func (h handler) CreateEvent(ctx context.Context, req *grpcModels.CreateEventReq
 }
 
 func (h handler) GetEvent(ctx context.Context, req *grpcModels.GetEventRequest) (*grpcModels.GetEventResponse, error) {
-	return nil, nil
-}
-func (h handler) DeleteEvent(ctx context.Context, req *grpcModels.DeleteEventRequest) (*grpcModels.DeleteEventResponse, error) {
-	return nil, nil
-}
-func (h handler) UpdateEvent(ctx context.Context, req *grpcModels.UpdateEventRequest) (*grpcModels.UpdateEventResponse, error) {
-	return nil, nil
-}
-func (h handler) ListEvents(ctx context.Context, req *grpcModels.ListEventsRequest) (*grpcModels.ListEventsResponse, error) {
-	return nil, nil
+	resp, err := h.eventController.GetEvent(ctx, h.adapt.getEventRequest(req))
+	if err != nil {
+		return nil, err
+	}
+	return h.adapt.getEventResponse(resp), nil
 }
 
-func NewHandler() grpcModels.ApiServiceServer {
-	return handler{}
+func (h handler) DeleteEvent(ctx context.Context, req *grpcModels.DeleteEventRequest) (*grpcModels.DeleteEventResponse, error) {
+	resp, err := h.eventController.DeleteEvent(ctx, h.adapt.deleteEventRequest(req))
+	if err != nil {
+		return nil, err
+	}
+	return h.adapt.deleteEventResponse(resp), nil
+}
+
+func (h handler) UpdateEvent(ctx context.Context, req *grpcModels.UpdateEventRequest) (*grpcModels.UpdateEventResponse, error) {
+	resp, err := h.eventController.UpdateEvent(ctx, h.adapt.updateEventRequest(req))
+	if err != nil {
+		return nil, err
+	}
+
+	return h.adapt.updateEventResponse(resp), nil
+}
+func (h handler) ListEvents(ctx context.Context, req *grpcModels.ListEventsRequest) (*grpcModels.ListEventsResponse, error) {
+	resp, err := h.eventController.ListEvents(ctx, h.adapt.listEventRequest(req))
+	if err != nil {
+		return nil, err
+	}
+
+	return h.adapt.listEventResponse(resp), nil
+}
+
+func NewHandler(cntrl eventController.Controller) grpcModels.ApiServiceServer {
+	return handler{
+		eventController:               cntrl,
+		adapt:                         adapter{},
+		UnimplementedApiServiceServer: grpcModels.UnimplementedApiServiceServer{},
+	}
 }
