9771811ec2f3f4f83ed69d2f8a85ebc3e38b4608
Aleksandr Rykalin <arykalin@gmail.com>
2022-09-22 19:50:34 +0300

    making grpc

--- pkg/server/v1/grpc/clients/client.go
+++ pkg/server/v1/grpc/clients/client.go
@@ -9,17 +9,47 @@ type Client interface {
 	Authenticate(request models.AuthenticateRequest) (models.AuthenticateResponse, error)
 	CreateEvent(request models.CreateEventRequest) (models.CreateEventResponse, error)
 	DeleteEvent(request models.DeleteEventRequest) (models.DeleteEventResponse, error)
-	GetEvent(params *APIServiceGetEventParams) (*APIServiceGetEventOK, error)
-	ListEvents(params *APIServiceListEventsParams) (*APIServiceListEventsOK, error)
-	UpdateEvent(params *APIServiceUpdateEventParams) (*APIServiceUpdateEventOK, error)
+	GetEvent(request models.GetEventRequest) (models.GetEventResponse, error)
+	ListEvents(request models.ListEventsRequest) (models.ListEventsResponse, error)
+	UpdateEvent(request models.UpdateEventRequest) (models.UpdateEventResponse, error)
 }
 
 type client struct {
-	grpcCli *generatedClient.Client
+	grpcCli *generatedClient.YearWheel
+}
+
+func (c client) Authenticate(request models.AuthenticateRequest) (models.AuthenticateResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c client) CreateEvent(request models.CreateEventRequest) (models.CreateEventResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c client) DeleteEvent(request models.DeleteEventRequest) (models.DeleteEventResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c client) GetEvent(request models.GetEventRequest) (models.GetEventResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c client) ListEvents(request models.ListEventsRequest) (models.ListEventsResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (c client) UpdateEvent(request models.UpdateEventRequest) (models.UpdateEventResponse, error) {
+	//TODO implement me
+	panic("implement me")
 }
 
 func NewClient() Client {
-	grpcCli := generatedClient.NewHTTPClient()
+	grpcCli := generatedClient.NewHTTPClient(nil)
 	return &client{
 		grpcCli: grpcCli,
 	}
